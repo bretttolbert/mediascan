cea8819ee2f29155e18ad7fe0418cd9a9e6665f8
Brett Tolbert <bretttolbert@gmail.com>
2022-12-14 19:58:04 -0600

    added year vs duration plot

--- mediastats.py
+++ mediastats.py
@@ -60,6 +60,9 @@ def get_file_sizes(data):
 def get_file_durations(data):
     return [int(f['duration']) for f in data['mediafiles']]
 
+def get_years(data):
+    return [int(f['year']) for f in data['mediafiles']]
+
 def plt_year_counts(data):
     counts = get_year_counts(data)
     df = pd.DataFrame({'year': counts.keys(), 'count': counts.values()})
@@ -80,22 +83,30 @@ def plt_file_sizes(data):
     plt.gca().set_xticklabels(["{:.2f}".format(x/conversion_unit) for x in current_values])
 
 def plt_file_durations(data):
-    sizes = get_file_durations(data)
-    plt.hist(sizes, bins=range(800), color='b', edgecolor='red')
+    durations = get_file_durations(data)
+    plt.hist(durations, bins=range(800), color='b', edgecolor='red')
     plt.xticks(rotation='vertical')
     plt.xlim(0, 800)
     plt.ylim(0, 100)
-    xtick_min = min(sizes)
+    xtick_min = min(durations)
     #xtick_max = max(sizes)+1
     xtick_max = 800
     plt.xticks(np.arange(xtick_min, xtick_max, 10.0))
+
+def plt_year_vs_duration(data):
+    years = get_years(data)
+    durations = get_file_durations(data)
+    plt.scatter(x=years, y=durations)
+    plt.xlim(1950, 2022)
+    plt.ylim(0, 3000)
 
 def main():
     data = load_yaml_file('files.yaml')
     #plt_year_counts(data)
     #plt_genre_counts(data)
     #plt_file_sizes(data)
-    plt_file_durations(data)
+    #plt_file_durations(data)
+    plt_year_vs_duration(data)
     plt.show()
 
 if __name__ == "__main__":
